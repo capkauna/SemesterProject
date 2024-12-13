f35bd7d2d93f9c4c2da554a37e12a8f522f5996e
Elina Grumbina <355013@via.dk>
2024-12-13 13:55:55 +0100

    added VIaPets class

---
+++ java/project/src/ViaPets.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+
+public class VIAPets {
+
+
+  public ArrayList<Customer> getAllCustomers() {
+    return new ArrayList<>();
+  }
+
+  public ArrayList<Booking> getAllBookings() {
+    return new ArrayList<>(); // Placeholder
+  }
+
+  public ArrayList<Sale> getAllPetsForSale() {
+    return new ArrayList<>();
+  }
+
+  public ArrayList<Pet> getAllPets() {
+    return new ArrayList<>();
+  }
+}
